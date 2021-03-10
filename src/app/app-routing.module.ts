f34970e8b221e554bea1893f461e4ceb911cb02c
saif31617 <48271551+saif31617@users.noreply.github.com>
2021-03-10 14:00:51 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{TestcomponentComponent} from './Components/testcomponent/testcomponent.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'login', component: TestcomponentComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
